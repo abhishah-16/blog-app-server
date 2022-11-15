c645b027951eea33ec5dd9e97828f5d6652fee81
abhishah <abhishah0961@gmail.com>
2022-11-15 11:01:30 +0530

    Added: CRUD for category

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -18,6 +18,12 @@ export class Category {
     @Column()
     description: string
 
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    updatedAt: Date
+
     @OneToMany(() => Post, (post) => post.category)
     posts: Post[]
 }
